c26f1ef3004dcb604119119a77402d3d7ed6f58d
hitode909 <hitode909@gmail.com>
2010-06-07 14:25:43 +0900

    --no-play, --from

--- cui8tracks.rb
+++ cui8tracks.rb
@@ -15,6 +15,8 @@ OptionParser.new {|opt|
   opt.on('-t TAG', '--tag')   {|v| $opts[:tag] = v}
   opt.on('-u USER', '--user')  {|v| $opts[:user] = v}
   opt.on('-s SORT', '--sort', '[recent|popular|random]')  {|v| $opts[:sort] = v}
+  opt.on('--no-play', "don't play tracks")  {|v| $opts[:no_play] = true}
+  opt.on('--from FROM', 'play from [FROM]th mix')  {|v| $opts[:from] = v.to_i}
   opt.on('--verbose', 'print mplayer output')  {|v| $opts[:verbose] = v}
   opt.on('--debug', 'debug-mode')  {|v| $opts[:debug] = v}
   opt.parse!(ARGV)
@@ -56,41 +58,49 @@ def json(path)
   JSON.parse(open(path, :http_basic_authentication => [$config['accesskey'], $config['secretkey']]).read)
 end
 
+def download_url_to_path(url, file_path)
+  $logger.info "downloading #{url}"
+  total = nil
+  open(file_path, 'w') {|local|
+    got = open(url,
+      :content_length_proc => proc{|_total|
+        total = _total
+      },
+      :progress_proc => proc{ |now|
+        print "%3d%% #{now}/#{total}\r" % (now/total.to_f*100)
+        $stdout.flush
+      }
+      ) {|remote|
+      local.write(remote.read)
+    }
+  }
+rescue => e
+  $logger.fatal "failed to download #{url}"
+  File.unlink(file_path) if File.exist?(file_path)
+  raise 'failed'
+end
+
 def prepare_file(url)
   unless File.directory?('cache')
     $logger.debug('make cache directory')
     Dir.mkdir('cache')
   end
+
   file_path = 'cache/' + url.gsub(File.extname(url), '').gsub(/[^\w]/, '_') + File.extname(url)
   if File.exist?(file_path)
     $logger.info "has cache: #{file_path}"
     return file_path
   end
 
-  Thread.new{
-    begin
-      $logger.info "downloading #{url}"
-      total = nil
-      open(file_path, 'w') {|local|
-        got = open(url,
-          :content_length_proc => proc{|_total|
-            total = _total
-          },
-          :progress_proc => proc{ |now|
-            print "%3d%% #{now}/#{total}\r" % (now/total.to_f*100)
-            $stdout.flush
-          }
-          ) {|remote|
-          local.write(remote.read)
-        }
-      }
-    rescue => e
-      $logger.fatal "failed to download #{url}"
-      File.unlink(file_path) if File.exist?(file_path)
-      exit 1
-    end
-  }
+  if $opts[:no_play]
+    download_url_to_path(url, file_path) rescue return nil
+    return 5
+    return file_path
+  end
 
+  Thread.new{
+    download_url_to_path(url, file_path) rescue nil
+  }
   return url
 end
 
@@ -100,6 +110,7 @@ def play(track)
   $logger.info "contributor: #{track['contributor']}"
   $logger.info "url: #{track['referenceUrl']}"
   path = prepare_file(track['item'])
+  return if $opts[:no_play]
   cmd = "mplayer #{path}"
   cmd += " >& /dev/null" unless $opts[:verbose]
   $logger.debug cmd
@@ -110,9 +121,15 @@ end
 
 set   = json("http://api.8tracks.com/sets/new.json")
 
+current = 0
+
 loop {
   mixes = json(mixes_path)
   mixes['mixes'].each_with_index{ |mix, index|
+    if $opts[:from] && current < $opts[:from]
+      current += 1
+      next
+    end
     index = ($opts[:page] - 1) * $opts[:per_page] + index + 1
     $logger.info "index: #{index} / #{mixes['total_entries']}"
     $logger.info "mix: #{mix['name']}"

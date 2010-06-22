aa383db10c59843da1defdcc84075b68eb3b1f8c
hitode909 <hitode909@gmail.com>
2010-06-22 20:39:25 +0900

    api class, login

--- cui8tracks.rb
+++ cui8tracks.rb
@@ -6,9 +6,84 @@ require 'pp'
 require 'logger'
 require 'pit'
 require 'optparse'
+require 'cgi'
 
 # methods
+module EightTracks
+  def logger
+    return @logger if @logger
+    @logger = Logger.new STDOUT
+    @logger.level = Logger::DEBUG
+    @logger
+  end
 
+  class API
+    include EightTracks
+    class LoginError < StandardError
+    end
+
+    def initialize(username, password)
+      @username = username
+      @password = password
+      @logged_in = false
+      login
+    end
+
+    def login
+      return if @logged_in
+      res = post('/sessions', :login => @username, :password => @password)
+      @logged_in = true if res['logged_in']
+    end
+
+    def to_param_str(hash)
+      raise ArgumentError, 'Argument must be a Hash object' unless hash.is_a?(Hash)
+      hash.to_a.map{|i| i[0].to_s + '=' + CGI.escape(i[1].to_s) }.join('&')
+    end
+
+    def http_request(klass, path, param = { })
+      path += '.json'
+      req = klass.new(path)
+      req.basic_auth(@username, @password) if @logged_in
+      param_str = to_param_str(param)
+      res = Net::HTTP.start('8tracks.com', 80) do |http|
+        if param_str
+          http.request(req, param_str)
+        else
+          http.request(req)
+        end
+      end
+      json_data = JSON.parse(res.body)
+      case res.code
+      when '200'
+        json_data
+      else
+        pp res
+        raise 'api'
+      end
+    end
+
+    def get(path, param = { })
+      http_request(Net::HTTP::Get, path, param)
+    end
+
+    def post(path, param = { })
+      http_request(Net::HTTP::Post, path, param)
+    end
+
+  end
+end
+
+pit = Pit.get('8tracks_api', :require => {
+    'username' => 'username',
+    'password' => 'password',
+  })
+
+api = EightTracks::API.new(pit['username'], pit['password'])
+sleep 3
+pp api.get('/mixes', :per_page => 1)
+sleep 3
+pp api.post('/mixes/14/toggle_like')
+exit
 
 def queries
   q = []

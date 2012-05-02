8702a83d6e9d088cfc8dae49c2861b7f48105709
dignati <dignati@dignati.(none)>
2012-05-02 15:13:52 +0200

    Never use STDOUT for logger output as we don't want to see it

--- lib/cui8tracks/session.rb
+++ lib/cui8tracks/session.rb
@@ -3,7 +3,7 @@ class CUI8Tracks::Session
 
   def logger
     return @logger if @logger
-    @logger = self.config[:debug] ? Logger.new(STDOUT) : Logger.new(".cui8tracks.log", File::WRONLY | File::APPEND | File::CREAT)
+    @logger = Logger.new(".cui8tracks.log", File::WRONLY | File::APPEND | File::CREAT)
     @logger.level = self.config[:debug] ? Logger::DEBUG : Logger::INFO
     @logger
   end
